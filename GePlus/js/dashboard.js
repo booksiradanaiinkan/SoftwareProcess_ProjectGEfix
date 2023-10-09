6da1f3b70e4777e49a76dbc6aa5f57210931ed80
BOOK DE PHARAOH <142472062+booksiradanaiinkan@users.noreply.github.com>
2023-10-10 02:53:36 +0700

    Update dashboard.js

--- GePlus/js/dashboard.js
+++ GePlus/js/dashboard.js
@@ -32,5 +32,63 @@ var images = document.querySelectorAll(".image");
     } 
 
     function closePopup() {
+        var commentContainer = document.getElementById("commentContainer");
+        
+        // ไม่ซ่อนคอนเทนเนอร์แชท
+        // แค่ปิด pop-up
         document.getElementById("popup").style.display = "none";
+    } 
+// JavaScript
+const slider = document.querySelector('.slider');
+let counter = 0;
+let autoSlideInterval;
+
+// ฟังก์ชันเลื่อน Slide
+function slide() {
+    if (counter < 0) {
+        counter = 2; // หากคลิก "ก่อนหน้า" จากหน้าแรกให้ไปที่รูปภาพสุดท้าย
     }
+    if (counter > 2) {
+        counter = 0; // หากคลิก "ถัดไป" จากหน้าสุดท้ายให้ไปที่รูปภาพแรก
+    }
+    slider.style.transform = `translateX(-${counter * 100}%)`;
+
+    // เพิ่มตรวจสอบเมื่อเลื่อนมาถึงรูปสุดท้าย
+    if (counter === 2) {
+        // หากเลื่อนมาถึงรูปสุดท้ายให้กลับไปที่รูปแรก
+        setTimeout(() => {
+            counter = 0;
+            slider.style.transition = 'none'; // ปิดการเปลี่ยนแปลงตำแหน่งด้วย transition
+            slider.style.transform = `translateX(-${counter * 100}%)`;
+            setTimeout(() => {
+                slider.style.transition = ''; // เปิดการเปลี่ยนแปลงตำแหน่งด้วย transition
+            }, 10);
+        }, 500); // รอเวลาเพื่อให้รูปสุดท้ายแสดงเป็นเวลาสั้นๆ ก่อนกลับไปที่รูปแรก
+    }
+}
+// เริ่มเลื่อน Slide อัตโนมัติ
+function startAutoSlide() {
+    autoSlideInterval = setInterval(() => {
+        counter++;
+        slide();
+    }, 3000); // เลื่อน Slide ทุก 5 วินาที (5000 มิลลิวินาที)
+}
+
+// หยุดเลื่อน Slide อัตโนมัติ
+function stopAutoSlide() {
+    clearInterval(autoSlideInterval);
+}
+
+// ฟังก์ชันเลื่อน Slide
+function slide() {
+    if (counter < 0) {
+        counter = 2; // หากคลิก "ก่อนหน้า" จากหน้าแรกให้ไปที่รูปภาพสุดท้าย
+    }
+    if (counter > 2) {
+        counter = 0; // หากคลิก "ถัดไป" จากหน้าสุดท้ายให้ไปที่รูปภาพแรก
+    }
+    slider.style.transform = `translateX(-${counter * 100}%)`;
+}
+
+// เริ่มเลื่อน Slide อัตโนมัติเมื่อโหลดหน้าเว็บ
+startAutoSlide();
